20bd1f4a147b28944d48eb92471c57b6959b145a
BrightBtw <diulsya96@gmail.com>
2025-11-13 03:05:24 +0500

    fourth lesson v2

--- js/sendForm.js
+++ js/sendForm.js
@@ -14,16 +14,27 @@ const sendForm = () => {
             email: email.value
         }
 
-        fetch('https://jsonplaceholder.typicode.com/posts', {
+        // fetch('https://jsonplaceholder.typicode.com/posts', {
+        fetch('https://jsonplaceholder.typicode.com/wrong-endpoint', {
+
             method: 'POST',
             body: JSON.stringify(sendObj),
             headers: {
                 'Content-type': 'application/json; charset=UTF-8',
             },
         })
-            .then((response) => response.json())
-            .then((json) => console.log(json))
+            .then(async (response) => {
+                if (response.ok) {
+                    const json = await response.json();
+                    return json;
+                } else {
+                    alert("Ошибка HTTP: " + response.status);
+                }
+            })
+            .then((json) =>
+                console.log(json))
             .finally(() => {
+                form.reset();
                 console.log('Форма очищена');
             })
     })
